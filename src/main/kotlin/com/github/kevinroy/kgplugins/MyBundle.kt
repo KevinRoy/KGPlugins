459f0a5b8b524bc19f7fdf1b02512f41a1a6f46a
GitHub Action <action@github.com>
2026-06-16 17:30:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/kevinroy/kgplugins/MyBundle.kt
+++ src/main/kotlin/com/github/kevinroy/kgplugins/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kevinroy.kgplugins
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kevinroy/kgplugins/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kevinroy/kgplugins/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kevinroy.kgplugins.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kevinroy.kgplugins.MyBundle
+import com.github.kevinroy.kgplugins.services.MyProjectService
 import javax.swing.JButton
 
 
